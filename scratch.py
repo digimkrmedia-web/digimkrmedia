24728b98ab37a1719612a2ba48a16755edadbfe0
digimkrmedia-web <digimkrmedia@gmail.com>
2026-05-05 14:03:02 +0530

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,6 @@
+import urllib.request
+import re
+req = urllib.request.Request('https://pin.it/DfR37ocVv', headers={'User-Agent': 'Mozilla/5.0'})
+html = urllib.request.urlopen(req).read().decode('utf-8')
+urls = re.findall(r'"contentUrl":"([^"]+)"', html)
+print(urls)
